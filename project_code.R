e3bcd48e704d5abb37e67582b73059bec3e3da46
Jim Leach <jimmy22theave@googlemail.com>
2015-01-21 17:06:03 +0000

    Update project_code.R
    Major additions with charts and new model testings

--- project_code.R
+++ project_code.R
@@ -22,6 +22,7 @@ library("dplyr", quietly = T)
 library("ggplot2", quietly = T)
 library("magrittr", quietly = T)
 library("GGally", quietly = T)
+library("MASS", quietly = T)
 
 dir <- "C:/Users/Jleach1/Documents/R/regression_models"
 setwd(dir)
@@ -32,12 +33,13 @@ rm(dir)
 data(mtcars)
 # mtcars <- mtcars %>%
 #           mutate(trans = as.factor(ifelse(am==0,"Automatic","Manual")))
-
 ################################################################################
 # Step 2 - initial explorations
 ################################################################################
+str(mtcars)
+
 summ_simple <- mtcars %>%
-                group_by(trans) %>%
+                group_by(am) %>%
                 summarise(mean_mpg = mean(mpg),
                          sd = sd(mpg),
                           n = n(),
@@ -47,7 +49,9 @@ summ_simple
 
 violin <-
 mtcars %>%
-  ggplot(aes(x = trans, y = mpg, fill = trans))+
+  ggplot(aes(x = as.factor(ifelse(am==0,"Automatic","Manual")),
+             y = mpg,
+             fill = as.factor(ifelse(am==0,"Automatic","Manual"))))+
   geom_violin()+
   geom_point()+
   theme_minimal()+
@@ -59,14 +63,112 @@ mtcars %>%
   xlab("Transmission Type")+
   ylab("Miles per Gallon")
 
-pairs <- ggpairs(mtcars, axisLabels = "internal", colour = "steelblue")
+pairs <- ggpairs(mtcars, axisLabels = "internal")
 
-fit_am <- lm(mpg ~ am, data = mtcars)
+################################################################################
+# Step 3 - clean up for model fit
+################################################################################
+mtcars$cyl <- factor(mtcars$cyl)
+mtcars$am <- factor(mtcars$am)
+mtcars$gear <- factor(mtcars$gear)
 
-fit_amResid <- fit_am$resid
+
+fit_all <- lm(mpg ~ . ,data = mtcars)
+summary(fit_all)
+
+step <- stepAIC(fit_all, direction = "both")
+step$anova
 
 
-fit_all <- lm(mpg ~ ., data = mtcars)
+
+fitRef <- lm(mpg ~ am + wt + qsec - 1, data = mtcars)
+summary(fitRef)$coef
+
+fitRef2 <- update(fitRef, . ~ . +am*qsec)
+summary(fitRef2)$coef
+
+fitRef3 <- update(fitRef, . ~ . +am*wt)
+summary(fitRef3)$coef
+
+fitRef4 <- update(fitRef, . ~ .+ wt*am + am*qsec + wt*qsec*am)
+summary(fitRef4)$coef
+
+aov2 <- anova(fitRef, fitRef2), fitRef3, fitRef4)
+
+aov3 <- anova(fitRef, fitRef3)
+aov4 <- anova(fitRef, fitRef2)
 
 
-fit_refined <- lm(mpg ~ wt+am, data = mtcars)
+
+
+fitRefInteract <- lm(mpg ~ am*(wt + qsec), data = mtcars)
+summary(fitRefInteract)$coef
+
+
+
+fitRefInteractScaled <- lm(scale(mpg) ~ scale(am)*(scale(wt) + scale(qsec)), data = mtcars)
+summary(fitRefInteractScaled)$coef
+
+
+
+mtcars %>%
+  ggplot(aes(x = wt, y = mpg, colour = factor(am)))+
+  geom_point()+
+  facet_grid(.~am)
+
+
+aov <- anova(fitRef, fitRefInteract)
+aov
+
+
+
+confint(fitRefInteract)
+
+fit <- lm(mpg ~ am, data = mtcars)
+confint(fit)
+
+fitRefRes <- fortify(fitRef)
+
+
+# resid vs. fitted
+residFit <- ggplot(fitRefRes, aes(.fitted, .resid)) +
+  geom_point(colour = "steelblue")  +
+  geom_smooth(method = "loess", se=FALSE, colour = "firebrick") +
+  geom_hline(linetype=2, size=.2) +
+  scale_x_continuous("Fitted Values") +
+  scale_y_continuous("Residual") +
+  ggtitle("Residuals vs Fitted")+
+  theme_minimal()
+
+# normal Q-Q
+a <- quantile(fitRefRes$.stdresid, c(0.25, 0.75))
+b <- qnorm(c(0.25, 0.75))
+slope <- diff(a)/diff(b)
+int <- a[1] - slope * b[1]
+qq <- ggplot(fitRefRes, aes(sample=.resid)) +
+  stat_qq(colour = "steelblue") +
+  geom_abline(slope=slope, intercept=int, colour = "firebrick") +
+  scale_x_continuous("Theoretical Quantiles") +
+  scale_y_continuous("Standardized Residuals") +
+  theme_minimal()+
+  ggtitle("Normal Q-Q")
+
+
+# Scale -location (stanardised resid vs. fitted)
+standFit <- ggplot(fitRefRes, aes(.fitted, sqrt(abs(.stdresid)))) +
+  geom_point(colour = "steelblue") +
+  geom_smooth(se=FALSE, method = "loess", colour = "firebrick") +
+  scale_x_continuous("Fitted Values") +
+  scale_y_continuous("Root of Standardized Residuals") +
+  theme_minimal()+
+  ggtitle("Scale-Location")
+
+# residuals vs. leverage
+residLev <- ggplot(fitRefRes, aes(.hat, .stdresid)) +
+  geom_point(colour = "steelblue") +
+  geom_smooth(se=FALSE, colour = "firebrick", method = "loess") +
+  geom_hline(linetype=2, size=.2) +
+  scale_x_continuous("Leverage") +
+  scale_y_continuous("Standardized Residuals")+
+  ggtitle("Residuals vs. Leverage")+
+  theme_minimal()
